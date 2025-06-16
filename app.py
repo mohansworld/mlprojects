fefa32979cc737e5292d96fc522917b27e947451
Rajatkumarsahu1 <93485116+Rajatkumarsahu1@users.noreply.github.com>
2025-06-16 13:06:45 +0530

    app.py

--- app.py
+++ app.py
@@ -108,3 +108,14 @@ with right_col:
         prediction = lm2.predict(input_array)
         st.subheader("Predicted Price")
         st.write(f"${prediction[0]:,.2f}")
+ # Footer
+    st.markdown("---", unsafe_allow_html=True)
+    st.markdown(
+        """
+        <div style='text-align: center;'>
+            <strong>Made by <a href='https://www.linkedin.com/in/rajat-kumar-sahu1/' target='_blank'>Rajat Kumar Sahu</a></strong><br>
+            📧 <a href='mailto:rajatks1997@gmail.com'>rajatks1997@gmail.com</a>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
